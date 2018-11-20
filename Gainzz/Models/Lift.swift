d14925132d95e03ccfaf6a23b779cdb93d3706ce
Wyatt Hurst <wdhurst@mavs.coloradomesa.edu>
2018-11-19 19:44:33 -0700

    database stuff

---
+++ Gainzz/Models/Lift.swift
@@ -0,0 +1,13 @@
+//
+//  Lift.swift
+//  Gainzz
+//
+//  Created by Wyatt Hurst on 11/19/18.
+//  Copyright © 2018 Wyatt Hurst. All rights reserved.
+//
+
+import UIKit
+
+class Lift: NSObject {
+
+}
